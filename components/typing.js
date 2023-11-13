d0b920a75acea250bb930d68ba50ffd8b72a5e29
lillian-tran <lilliantran17@gmail.com>
2023-11-13 16:42:15 -0500

    typing speeds and color icon

--- components/typing.js
+++ components/typing.js
@@ -7,7 +7,7 @@ import Typewriter from 'typewriter-effect/dist/core';
 export default function Typing() {
     return (
         <div className={typeStyles.heading}>
-            <h1>Jumbo<span id='typed'>{type()}</span></h1>
+            <h1>Jumbo<span id='typed' className={typeStyles.typed}>{type()}</span></h1>
         </div>
     )
 }
@@ -19,19 +19,17 @@ function type(){
 
         var typewriter = new Typewriter(app, {
         loop: true,
-        delay: 75,
         });
 
         typewriter
-        .pauseFor(250)
         .typeString('<span style ="color: #C6302C;">Hack</span>')
-        .pauseFor(800)
-        .deleteChars(10)
+        .pauseFor(1800)
+        .deleteChars(9)
         .typeString('<span style ="color: #C6302C;">Code</span>')
-        .pauseFor(800)
-        .deleteChars(10)
+        .pauseFor(1000)
+        .deleteChars(8)
         .typeString('<span style ="color: #C6302C;">Love</span>')
-        .pauseFor(300)
+        .pauseFor(800)
         .start();
     });
     
