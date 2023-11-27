e5cf84785183ea956545b341db7f16c241f0dde6
Tyler Thompson <tyler.thompson@tufts.edu>
2023-11-26 19:21:10 -0500

    Fix dates

--- components/subtitle.js
+++ components/subtitle.js
@@ -4,7 +4,7 @@ export default function Subtitle() {
   return (
     <div className={styles.container}>
       <div className={styles.subtitleText}>
-        <h3>Feb 24th-25th 2024</h3>
+        <h3>Feb 17th-18th 2024</h3>
         <h3>Tufts University</h3>
       </div>
       <div className={styles.application}>
