92bf560aec1dfed6aee791a7d87a8cd87922696d
Tyler Thompson <tyler.thompson@tufts.edu>
2023-12-03 17:21:14 -0500

    Actually ACTUALLY fix roboto

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className={roboto.className}>{children}</body>
     </html>
   );
